00823040115201498b1533c300a7b78315e0f604
leo <leohthom@gmail.com>
2023-07-21 23:36:17 +0800

    Show 2 cards on mobile

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ movies }) => {
   return (
-    <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {movies &&
         movies
           .filter((movie) => movie.poster_path)
--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -3,7 +3,7 @@ import PersonCard from "./PersonCard";
 
 const PeopleList = ({ people }) => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {people.map((person, index) => (
         <PersonCard key={index} person={person} />
       ))}
