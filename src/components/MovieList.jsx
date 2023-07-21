671ada05a2936203a1dbc780b43a91a873475607
leo <leohthom@gmail.com>
2023-07-21 23:45:34 +0800

    Centered MovieList and PeopleList items

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCard";
 
 const MovieList = ({ movies }) => {
   return (
-    <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center items-start">
       {movies &&
         movies
           .filter((movie) => movie.poster_path)
--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -3,7 +3,7 @@ import PersonCard from "./PersonCard";
 
 const PeopleList = ({ people }) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center items-start">
       {people.map((person, index) => (
         <PersonCard key={index} person={person} />
       ))}
