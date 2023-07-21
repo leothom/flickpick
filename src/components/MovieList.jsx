7804ea434cd2be537129046dc628cf4c2a08c1e1
leo <leohthom@gmail.com>
2023-07-22 00:01:31 +0800

    Added "No results found" text

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,10 +4,15 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ movies }) => {
   return (
     <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center items-start">
-      {movies &&
+      {movies && movies.length > 0 ? (
         movies
           .filter((movie) => movie.poster_path)
-          .map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+          .map((movie) => <MovieCard key={movie.id} movie={movie} />)
+      ) : (
+        <p className="w-full text-center mt-4 text-xl text-gray-500">
+          No results found.
+        </p>
+      )}
     </div>
   );
 };
--- src/components/PeopleList.jsx
+++ src/components/PeopleList.jsx
@@ -3,10 +3,16 @@ import PersonCard from "./PersonCard";
 
 const PeopleList = ({ people }) => {
   return (
-    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-items-center items-start">
-      {people.map((person, index) => (
-        <PersonCard key={index} person={person} />
-      ))}
+    <div className="grid gap-4 grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center items-start">
+      {people && people.length > 0 ? (
+        people
+          .filter((person) => person.profile_path)
+          .map((person) => <PersonCard key={person.id} person={person} />)
+      ) : (
+        <p className="w-full text-center mt-4 text-xl text-gray-500">
+          No results found.
+        </p>
+      )}
     </div>
   );
 };
